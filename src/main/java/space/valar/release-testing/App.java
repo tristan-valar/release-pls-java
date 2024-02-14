190dd1819335fbf5c8f2fa1e28d75067fa4bb61e
tristan <tm@tristanmayo.com>
2024-02-14 12:24:26 +0100

    feat: initial commit

---
+++ src/main/java/space/valar/release-testing/App.java
@@ -0,0 +1,13 @@
+package space.valar.release-testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
